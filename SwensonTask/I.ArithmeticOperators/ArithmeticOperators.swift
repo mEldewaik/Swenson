f838bb2bea57e0ac64e8281a76b120e25bfb9efe
mohamed eldewaik <mohamedeldewaik@gmail.com>
2021-06-28 22:40:33 +0200

    Arithmatic Operations

---
+++ SwensonTask/I.ArithmeticOperators/ArithmeticOperators.swift
@@ -0,0 +1,22 @@
+//
+//  ArithmeticOperators.swift
+//  SwensonTask
+//
+//  Created by Mohamed Eldewaik on 28/06/2021.
+//
+
+import Foundation
+
+/*
+ I. Add arithmetic operators (add, subtract, multiply, divide) to make the following expressions true. You can
+ use any parentheses you’d like.
+ 3 1 3 9 = 12
+ 
+ Solution:
+ 
+ 3 1 3 9 = (3+1) / (3/9)
+         = 4 / (1/3)
+         = 4 * 3
+         = 12
+ 
+  */
